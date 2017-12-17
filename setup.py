a4788944c6439dd102111d83f76b6755fbdfd3b4
WetDesertRock <wetdesertrock@gmail.com>
2017-12-16 20:22:42 -0700

    setup.py'ify it

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = "lovepacker",
+    packages = ["lovepacker"],
+    version = "1.0.3",
+    description = "Packages up LOVE games for distribution. Currently handles MacOS, Windows, and .love files.",
+    author = "WetDesertRock",
+    author_email = "wetdesertrock@gmail.com",
+    url = "https://github.com/WetDesertRock/lovepacker",
+    download_url = "https://github.com/WetDesertRock/lovepacker/archive/1.0.3.tar.gz",
+    keywords = ["love", "love2d", "packager", "package"],
+    classifiers = [],
+    install_requires = ['zipfile2>=0.0.12'],
+    scripts=['bin/lovepacker']
+)
