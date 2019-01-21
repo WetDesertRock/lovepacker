f7316ceb4bd560faee8dda038dc5901686b4e129
WetDesertRock <wetdesertrock@gmail.com>
2019-01-21 06:41:21 -0700

    Add guard to prevent python2 installation

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
 from distutils.core import setup
-setup(
-    name = "lovepacker",
-    packages = ["lovepacker"],
-    version = "1.0.4",
-    description = "Packages up LOVE games for distribution. Currently handles MacOS, Windows, and .love files.",
-    author = "WetDesertRock",
-    author_email = "wetdesertrock@gmail.com",
-    url = "https://github.com/WetDesertRock/lovepacker",
-    download_url = "https://github.com/WetDesertRock/lovepacker/archive/1.0.4.tar.gz",
-    keywords = ["love", "love2d", "packager", "package"],
-    classifiers = [],
-    install_requires = ['zipfile2>=0.0.12'],
-    scripts=['bin/lovepacker']
-)
+
+def main():
+    if sys.version_info[0] != 3:
+        sys.exit("This script requires Python 3")
+
+    setup(
+        name = "lovepacker",
+        packages = ["lovepacker"],
+        version = "1.0.5",
+        description = "Packages up LOVE games for distribution. Currently handles MacOS, Windows, and .love files.",
+        author = "WetDesertRock",
+        author_email = "wetdesertrock@gmail.com",
+        url = "https://github.com/WetDesertRock/lovepacker",
+        download_url = "https://github.com/WetDesertRock/lovepacker/archive/1.0.4.tar.gz",
+        keywords = ["love", "love2d", "packager", "package"],
+        classifiers = [],
+        install_requires = ['zipfile2>=0.0.12'],
+        scripts=['bin/lovepacker']
+    )
+
+
+if __name__ == "__main__":
+    main()
